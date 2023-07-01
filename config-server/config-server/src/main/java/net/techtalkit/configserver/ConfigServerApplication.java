ce22c7e39f2a650c496e00fe23ad24a485507e9b
techtalkitco <techtalkitco@gmail.com>
2023-07-01 19:58:35 +0530

    department and employee service refactor

--- config-server/config-server/src/main/java/net/techtalkit/configserver/ConfigServerApplication.java
+++ config-server/config-server/src/main/java/net/techtalkit/configserver/ConfigServerApplication.java
@@ -4,14 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableConfigServer
 @EnableEurekaClient
 public class ConfigServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
-
 }
