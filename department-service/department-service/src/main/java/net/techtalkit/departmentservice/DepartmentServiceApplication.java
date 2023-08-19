a7b19344d81520a07ee98300ce1b463d7c643cd7
techtalkitco <techtalkitco@gmail.com>
2023-08-19 14:08:27 +0530

    Swagger documentation for department service added

--- department-service/department-service/src/main/java/net/techtalkit/departmentservice/DepartmentServiceApplication.java
+++ department-service/department-service/src/main/java/net/techtalkit/departmentservice/DepartmentServiceApplication.java
@@ -1,11 +1,36 @@
 package net.techtalkit.departmentservice;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 //@EnableEurekaClient
+@OpenAPIDefinition(
+		info=@Info(
+				title="Department Service Rest APIs",
+				description = "Department Service Rest Api Documentation",
+				version="v1.0",
+				contact=@Contact(
+						name="Shafaat",
+						email="techtalkitco@gmail.com",
+						url="https://www.javaguides.com"
+				),
+				license = @License(
+						name="Apache 2.0",
+						url="www.techtalkit.co"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Department service doc",
+				url="www.techtalkit.com"
+		)
+)
 public class DepartmentServiceApplication {
 
 	public static void main(String[] args) {
--- department-service/department-service/src/main/java/net/techtalkit/departmentservice/controller/DepartmentController.java
+++ department-service/department-service/src/main/java/net/techtalkit/departmentservice/controller/DepartmentController.java
@@ -1,5 +1,8 @@
 package net.techtalkit.departmentservice.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import net.techtalkit.departmentservice.dto.DepartmentDto;
 import net.techtalkit.departmentservice.service.DepartmentService;
@@ -7,18 +10,40 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(
+        name="Department Service- DepartmentController",
+        description = "Department Controller exposes Rest APIs for department service"
+)
 @RestController
 @RequestMapping("api/departments")
 @AllArgsConstructor
 public class DepartmentController {
     private DepartmentService departmentService;
     //Build save department REST API
+    @Operation(
+            summary = "Save department Rest API",
+            description = "Save department Rest API is used to save department object in DB"
+
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status 201 Created"
+    )
     @PostMapping
     public ResponseEntity<DepartmentDto> saveDepartment(@RequestBody DepartmentDto departmentDto){
         DepartmentDto savedDepartment=departmentService.saveDepartment(departmentDto);
         return new ResponseEntity<>(savedDepartment, HttpStatus.CREATED);
     }
     //Build get department Rest API
+    @Operation(
+            summary = "Get department by code Rest API",
+            description = "Get department by code Rest API is used to get department from DB"
+
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 OK"
+    )
     @GetMapping("{department-code}")
     public ResponseEntity<DepartmentDto> getDepartment(@PathVariable("department-code") String departmentCode){
         DepartmentDto departmentDto=departmentService.getDepartmentByCode(departmentCode);
